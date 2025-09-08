15dacd6e37caed493ad6327f887f395374b66037
Jhon Mantilla <analistadesarrollo2@sasaconsultoria.com>
2025-09-08 17:24:51 -0500

    Ajustar ruta de la / a actualizacion/

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Hace que todas las rutas estén bajo /actualizacion
+  basePath: "/actualizacion",
+
+  // Prefijo de assets (chunks, css, imágenes públicas)
+  assetPrefix: "/actualizacion/",
+
+  // Genera build lista para docker
+  output: "standalone",
 };
-module.exports = {
-  output: 'standalone',
-}
+
 export default nextConfig;
